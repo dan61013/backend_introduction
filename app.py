ef3744f872c5f1005652d93d9413eb872c575d71
Dan Cai <zxc61013@gmail.com>
2022-11-18 17:09:42 +0800

    Flask, route, render_template, redirect

--- app.py
+++ app.py
@@ -1,18 +1,31 @@
 from flask import Flask
+from flask import url_for # 回傳路徑
+from flask import redirect # 重新導向
+from flask import render_template # 渲染頁面
 
 app = Flask(__name__)
+# app.config.from_pyfile("configs.py")
 
-# CONFIGS = {
-#     "DEBUG": True
-# }
+@app.route("/") # 根目錄
+def home():
+    return "Hello World!"
 
-app.config.from_pyfile('configs.py')
-# app.config['DEBUG'] = True
+@app.route("/test1")
+def test1():
+    # return url_for('home')
+    return redirect('http://127.0.0.1:5000/')
 
-@app.route("/")
-def Hello():
-    return "Hello world!"
+@app.route("/test2")
+def test2():
+    return redirect(url_for('test1'))
+
+@app.route("/index")
+def index():
+    return render_template('index.html', username='Dan')
+
+@app.route("/<user>")
+def test3(user):
+    return render_template('index.html', username='Dan')
 
 if __name__ == '__main__':
-    # app.debug = True
     app.run()
